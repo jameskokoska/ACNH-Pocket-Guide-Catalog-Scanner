a5cd9709e941dd83dac0361fa52d150388281869
jameskokoska <50821962+jameskokoska@users.noreply.github.com>
2023-01-02 14:27:27 -0500

    added version number

--- catalogscanner/lib/pages/home_page.dart
+++ catalogscanner/lib/pages/home_page.dart
@@ -738,13 +738,34 @@ Future<void> _infoDaialog(context, setPage) async {
                   ),
                 ],
               ),
-              const SizedBox(height: 15),
+              TextFont(
+                text:
+                    "v${packageInfoGlobal.version}+${packageInfoGlobal.buildNumber}",
+                textColor: Theme.of(context).colorScheme.tertiary,
+                textAlign: TextAlign.center,
+                fontSize: 14,
+              ),
+              const SizedBox(height: 20),
               Tappable(
                 color: Theme.of(context).colorScheme.primary,
                 borderRadius: 10,
                 onTap: () {
                   showLicensePage(
                       context: context,
+                      applicationIcon: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          width: 150,
+                          height: 70,
+                          child: Image(
+                            image: AssetImage('assets/images/app-icon.png'),
+                          ),
+                        ),
+                      ),
+                      applicationName:
+                          "Catalog Scanner\n(for ACNH Pocket Guide)",
+                      applicationVersion:
+                          "v${packageInfoGlobal.version}+${packageInfoGlobal.buildNumber}",
                       applicationLegalese:
                           "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.");
                 },
